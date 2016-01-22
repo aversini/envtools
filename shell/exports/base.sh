07c382ad73c6d7f42118db865eeec01965f1fcfb
Arno V <versini@gmail.com>
2016-01-21 18:00:15 -0800

    Adding Maven installation

--- shell/exports/base.sh
+++ shell/exports/base.sh
@@ -1,7 +1,15 @@
+export RUNTIME_DIR
 export ENVTOOLS_VERSION="$(cat ${ENVDIR}/../version)"
 export LANG=C
 export EDITOR=vi
 export PATH=$PATH:/usr/sbin
+
+# Maven
+if [ -d "$RUNTIME_DIR/apache-maven-3.3.9" ]; then
+  export M2_HOME="$RUNTIME_DIR/apache-maven-3.3.9"
+  export M2=$M2_HOME/bin
+  export PATH=$M2:$PATH
+fi
 
 if [ "${OS}" = "Darwin" ]; then
   source "${ENVDIR}/exports/mac.sh"
--- shell/exports/mac.sh
+++ shell/exports/mac.sh
@@ -6,12 +6,5 @@ fi
 # Java
 export JAVA_HOME="$(/usr/libexec/java_home)"
 
-# Maven
-if [ -d "$HOME/Library/Maven/Home" ]; then
-  export M2_HOME=$HOME/Library/Maven/Home
-  export M2=$M2_HOME/bin
-  export PATH=$M2:$PATH
-fi
-
 # Brew
 export PATH=/usr/local/bin:$PATH
