bd8a8c26f606fa152f6fb2a9660bca7fc0649ff5
Arno V <versini@gmail.com>
2016-08-17 13:01:23 -0700

    Bumping maven version to 3.3.9 since it's now supported

--- shell/exports/base.sh
+++ shell/exports/base.sh
@@ -10,6 +10,11 @@ if [ -d "$RUNTIME_DIR/apache-maven-3.2.5" ]; then
   export M2=$M2_HOME/bin
   export PATH=$M2:$PATH
 fi
+if [ -d "$RUNTIME_DIR/apache-maven-3.3.9" ]; then
+  export M2_HOME="$RUNTIME_DIR/apache-maven-3.3.9"
+  export M2=$M2_HOME/bin
+  export PATH=$M2:$PATH
+fi
 
 if [ "${OS}" = "Darwin" ]; then
   source "${ENVDIR}/exports/mac.sh"
