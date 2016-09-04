5a63e39e9341a90cd1a209db4399321e43a34f9e
Arno V <versini@gmail.com>
2016-09-04 10:23:35 -0700

    Hiding mac only help on non-mac browsers

--- data/assets/js/envtools.js
+++ data/assets/js/envtools.js
@@ -2,8 +2,14 @@
 
 $(function () {
   hljs.initHighlightingOnLoad();
+
+
   $('.envtools-tabs a').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
   });
+
+  if (navigator.userAgent.match(/mac os x/i)) {
+    $('.mac-only').removeClass('mac-only');
+  }
 });
