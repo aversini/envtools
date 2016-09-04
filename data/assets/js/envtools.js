bf20b4fdae9fc317f31ce2c96c05ea86e3fc62d2
Arno V <versini@gmail.com>
2016-09-04 16:15:26 -0700

    Adding hidden query string option to help page for debug

--- data/assets/js/envtools.js
+++ data/assets/js/envtools.js
@@ -1,15 +1,38 @@
 /*eslint-disable*/
 
+function parseQueryString(query) {
+  var
+    parser = /([^=?&]+)=?([^&]*)/g,
+    result = {},
+    part;
+
+  for (; part = parser.exec(query); result[decodeURIComponent(part[1])] = decodeURIComponent(part[2]));
+  return result;
+}
+
 $(function () {
+  var
+    search;
+
   hljs.initHighlightingOnLoad();
+  search = Object.keys(parseQueryString(document.location.search));
 
+  // hide content for mac if not a mac browser
+  if (navigator.userAgent.match(/mac os x/i)) {
+    $('.mac-only').removeClass('mac-only');
+  }
 
+  // go to the tab listed in the query string if any
+  if (search && search.length) {
+    // select tab by name
+    $('.envtools-tabs a[href="#' + search[0] + '"]').tab('show')
+  }
+
+  // activate tab navigation
   $('.envtools-tabs a').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
   });
 
-  if (navigator.userAgent.match(/mac os x/i)) {
-    $('.mac-only').removeClass('mac-only');
-  }
+
 });
