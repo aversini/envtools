04a97cd626364f44b0ef8c1a14f5d3cbe3455332
Arno V <versini@gmail.com>
2016-09-17 11:00:31 -0700

    Optimized version checking and getting rid of "request" dependency!

--- bin/check.js
+++ bin/check.js
@@ -5,32 +5,32 @@ var
   latest,
   moment = require('moment'),
   config = require('fedtools-config'),
-  request = require('request'),
+  download = require('download'),
   url = 'https://github.com/aversini/versions/raw/master/versions.json';
 
-request({
-  url: url,
-  timeout: 30000
-}, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    if (body) {
-      res = JSON.parse(body, 'utf8');
-      if (res && res.envtools && res.envtools.version) {
-        latest = res.envtools.version;
-
-        // need to mark version as checked
-        config.setKey('envtoolsVersion', {
-          expiration: moment().add(1, 'd'),
-          latest: latest
-        }, true, true);
-
-      }
-    }
-  } else {
-    process.exit(1);
-  }
-
+function _terminate () {
   // Call process exit explicitly to terminate the child process
   // Otherwise the child process will run forever.
   process.exit();
+}
+
+download(url, {
+  timeout: 30000,
+  retries: 0
+}).then(function (body) {
+  if (body) {
+    res = JSON.parse(body, 'utf8');
+    if (res && res.envtools && res.envtools.version) {
+      latest = res.envtools.version;
+      // need to mark version as checked and push
+      // the expiration date to 1 day
+      config.setKey('envtoolsVersion', {
+        expiration: moment().add(1, 'd'),
+        latest: latest
+      }, true, true);
+    }
+  }
+  _terminate();
+}).catch(function () {
+  _terminate();
 });
