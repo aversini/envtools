b01a7abef935067cd470386e95ea614bd122a3c7
Arno V <versini@gmail.com>
2016-10-14 11:04:48 -0700

    Prevent double load (if init script is called more than once in same session)

--- shell/load.sh
+++ shell/load.sh
@@ -1,98 +1,103 @@
 #!/bin/bash
 
-
-# Setting some constants available at load time
-# and within all scripts and functions sourced here.
-OS=$(uname)
-if [ "$ENVTOOLS_ENVDIR" != "" ]; then
-  ENVDIR=${ENVTOOLS_ENVDIR}
+# If already loaded and it's not a manual reload, get
+# out of here quietly...
+if [ "$INIT_PARAM" != "reload" -a "$RUNTIME_DIR" != "" ]; then
+  return
 else
-  ENVDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-fi
-RUNTIME_DIR="${HOME}/.envtools"
-CUSTOM_ENVDIR="${RUNTIME_DIR}/custom"
-TOOLSDIR="${ENVDIR}/../lib"
-OLD_PS1="${PS1}"
-# The proxy value should be saved
-# in the file "${RUNTIME_DIR}/proxy".
-# Let's read it at load time and save it.
-if [ -f "${RUNTIME_DIR}/proxy" ]; then
-  PROXY=`cat "${RUNTIME_DIR}/proxy"`
-else
-  PROXY=""
-fi
+  # Setting some constants available at load time
+  # and within all scripts and functions sourced here.
+  OS=$(uname)
+  if [ "$ENVTOOLS_ENVDIR" != "" ]; then
+    ENVDIR=${ENVTOOLS_ENVDIR}
+  else
+    ENVDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  fi
+  RUNTIME_DIR="${HOME}/.envtools"
+  CUSTOM_ENVDIR="${RUNTIME_DIR}/custom"
+  TOOLSDIR="${ENVDIR}/../lib"
+  OLD_PS1="${PS1}"
+  # The proxy value should be saved
+  # in the file "${RUNTIME_DIR}/proxy".
+  # Let's read it at load time and save it.
+  if [ -f "${RUNTIME_DIR}/proxy" ]; then
+    PROXY=`cat "${RUNTIME_DIR}/proxy"`
+  else
+    PROXY=""
+  fi
 
-# Overriding sudo for non-root users to carry the environemnt
-# over while sudoing.
-if [ $UID != 0 ]; then
-  MYSUDO="sudo -E"
-else
-   MYSUDO=""
-fi
+  # Overriding sudo for non-root users to carry the environemnt
+  # over while sudoing.
+  if [ $UID != 0 ]; then
+    MYSUDO="sudo -E"
+  else
+     MYSUDO=""
+  fi
 
-if [ "${OS}" == "Darwin" -o "${OS}" == "Linux" -o "${OS}" == "MINGW32_NT-6.1" ]; then
-  # Loading some functions
-  source "${ENVDIR}/third/git-prompt.sh"
-  source "${ENVDIR}/functions/prompt.sh"
-  source "${ENVDIR}/functions/base.sh"
-  source "${ENVDIR}/functions/logs.sh"
+  if [ "${OS}" == "Darwin" -o "${OS}" == "Linux" -o "${OS}" == "MINGW32_NT-6.1" ]; then
+    # Loading some functions
+    source "${ENVDIR}/third/git-prompt.sh"
+    source "${ENVDIR}/functions/prompt.sh"
+    source "${ENVDIR}/functions/base.sh"
+    source "${ENVDIR}/functions/logs.sh"
 
-  source "${ENVDIR}/functions/cmd.sh"
-  source "${ENVDIR}/functions/killers.sh"
-  source "${ENVDIR}/functions/proxy.sh"
+    source "${ENVDIR}/functions/cmd.sh"
+    source "${ENVDIR}/functions/killers.sh"
+    source "${ENVDIR}/functions/proxy.sh"
 
-  # Setting some aliases
-  source "${ENVDIR}/aliases/base.sh"
+    # Setting some aliases
+    source "${ENVDIR}/aliases/base.sh"
 
-  # Exporting some constants for future terminal use
-  source "${ENVDIR}/exports/base.sh"
+    # Exporting some constants for future terminal use
+    source "${ENVDIR}/exports/base.sh"
 
-  # Set proxy quietly
-  setProxyAtLoadTime
+    # Set proxy quietly
+    setProxyAtLoadTime
 
-  # Set the envtools custom prompt if the user asked for it
-  if [ -f "${RUNTIME_DIR}/envtools-prompt" ]; then
-    CUSTOM_PROMPT_TYPE=`cat "${RUNTIME_DIR}/envtools-prompt"`
-    if [ "$CUSTOM_PROMPT_TYPE" == "2" ]; then
-      setEnvtoolsPromptConfigurationSinopia
-    else
-      setEnvtoolsPromptConfigurationDefault
+    # Set the envtools custom prompt if the user asked for it
+    if [ -f "${RUNTIME_DIR}/envtools-prompt" ]; then
+      CUSTOM_PROMPT_TYPE=`cat "${RUNTIME_DIR}/envtools-prompt"`
+      if [ "$CUSTOM_PROMPT_TYPE" == "2" ]; then
+        setEnvtoolsPromptConfigurationSinopia
+      else
+        setEnvtoolsPromptConfigurationDefault
+      fi
+      PROMPT_COMMAND=setEnvtoolsPrompt
     fi
-    PROMPT_COMMAND=setEnvtoolsPrompt
-  fi
 
-  # Bid thee welcome
-  displayWelcomeBanner
+    # Bid thee welcome
+    displayWelcomeBanner
 
-  # if resume auto file is found, restart envtools auto.
-  if [ -f "${RUNTIME_DIR}/resume_auto" ]; then
-    envtools auto
-  fi
+    # if resume auto file is found, restart envtools auto.
+    if [ -f "${RUNTIME_DIR}/resume_auto" ]; then
+      envtools auto
+    fi
 
-  # Loading custom fuctions
-  if [ -f "${RUNTIME_DIR}/custom/functions.sh" ]; then
-    source "${RUNTIME_DIR}/custom/functions.sh"
-  fi
-  # Loading custom aliases
-  if [ -f "${RUNTIME_DIR}/custom/aliases.sh" ]; then
-    source "${RUNTIME_DIR}/custom/aliases.sh"
-  fi
-  # Loading custom exports
-  if [ -f "${RUNTIME_DIR}/custom/exports.sh" ]; then
-    source "${RUNTIME_DIR}/custom/exports.sh"
-  fi
+    # Loading custom fuctions
+    if [ -f "${RUNTIME_DIR}/custom/functions.sh" ]; then
+      source "${RUNTIME_DIR}/custom/functions.sh"
+    fi
+    # Loading custom aliases
+    if [ -f "${RUNTIME_DIR}/custom/aliases.sh" ]; then
+      source "${RUNTIME_DIR}/custom/aliases.sh"
+    fi
+    # Loading custom exports
+    if [ -f "${RUNTIME_DIR}/custom/exports.sh" ]; then
+      source "${RUNTIME_DIR}/custom/exports.sh"
+    fi
 
-  # in case of Mac and default terminal app, to allow opening a new tab
-  # in the same current folder, we need to trick the PROMPT_COMMAND a
-  # little bit
-  if [ "${OS}" == "Darwin" -a "${PROMPT_COMMAND}" != "" ]; then
-    if type update_terminal_cwd > /dev/null 2>&1 ; then
-      if ! [[ $PROMPT_COMMAND =~ (^|;)update_terminal_cwd($|;) ]] ; then
-        export PROMPT_COMMAND="$PROMPT_COMMAND;update_terminal_cwd"
+    # in case of Mac and default terminal app, to allow opening a new tab
+    # in the same current folder, we need to trick the PROMPT_COMMAND a
+    # little bit
+    if [ "${OS}" == "Darwin" -a "${PROMPT_COMMAND}" != "" ]; then
+      if type update_terminal_cwd > /dev/null 2>&1 ; then
+        if ! [[ $PROMPT_COMMAND =~ (^|;)update_terminal_cwd($|;) ]] ; then
+          export PROMPT_COMMAND="$PROMPT_COMMAND;update_terminal_cwd"
+        fi
       fi
     fi
-  fi
 
-else
-  echo "OS not supported/recognized... ($OS)"
+  else
+    echo "OS not supported/recognized... ($OS)"
+  fi
 fi
