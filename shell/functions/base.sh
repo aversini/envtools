57b1e40cc608715b02458df45327c5d6824c0520
Arno V <versini@gmail.com>
2017-07-12 11:02:53 -0700

    chore: allowing win7 64 bits

--- shell/functions/base.sh
+++ shell/functions/base.sh
@@ -33,7 +33,7 @@ function isWindows {
   if [ "$LOCAL_OS" == "" ]; then
     LOCAL_OS=$(uname)
   fi
-  [[ "$LOCAL_OS" == "MINGW32_NT-6.1" || "$LOCAL_OS" == "MINGW64_NT-10.0" ]]
+  [[ "$LOCAL_OS" == "MINGW32_NT-6.1" || "$LOCAL_OS" == "MINGW64_NT-6.1" || "$LOCAL_OS" == "MINGW64_NT-10.0" ]]
 }
 function isMac {
   local LOCAL_OS=$OS
