eed12f712022b2b8b4f5b7d5059377f6cfed2115
Arno V <versini@gmail.com>
2018-01-13 10:46:03 -0800

    feat: migrating to ES6
    - using babel compilation
    - raised support to only node 6 and higher

--- bin/envtools.js
+++ bin/envtools.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 
-/**
- * This is the internal Envtools API documentation. It is not intended to be
- * used by Envtools users, but rather by Envtools developers.
- * It is an easily accessible repository of all the methods
- * available in one centralized document (automatically generated
- * via yuidoc).
- *
- * @module envtools
- */
-
-require('../lib/cli/cli').parseCommandLine();
+require('../dist/cli/cli').parseCommandLine();
--- shell/load.sh
+++ shell/load.sh
@@ -47,7 +47,7 @@ fi
 RUNTIME_DIR="${HOME}/.envtools"
 RUNTIME_BIN_DIR="${HOME}/.envtools/bin"
 CUSTOM_ENVDIR="${RUNTIME_DIR}/custom"
-TOOLSDIR="${ENVDIR}/../lib"
+TOOLSDIR="${ENVDIR}/../dist"
 OLD_PS1="${PS1}"
 # The proxy value should be saved
 # in the file "${RUNTIME_DIR}/proxy".
