2a54aadbf447229ee1972b33a882be537e1cf358
Arno V <versini@gmail.com>
2018-01-20 10:35:53 -0800

    feat: first pass at using Jest for tests

---
+++ src/__tests__/isAppInstalled.js
@@ -0,0 +1,9 @@
+const isAppInstalled = require('../utilities/isAppInstalled');
+
+test('node is installed', () => {
+  expect(isAppInstalled('node')).toBeTruthy();
+});
+
+test('not-an-app is not installed', () => {
+  expect(isAppInstalled('not-an-app')).toBe(false);
+});
